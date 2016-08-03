16b72ce482a379472a392fd43a88466d2cf5dc24
AdrianPotter <adrian.potter@tessella.com>
2016-08-03 09:55:04 +0100

    Make sample changer a single app with variable dimension

--- linearSampleChangerApp/sample_config/NDWXXXX/configurations/galil/linearSampleChanger.cmd
+++
@@ -1,8 +0,0 @@
-# $(ICPCONFIGDIR) or $(ICPCONFIGROOT) ?
-$(IFDMC08) epicsEnvSet "LIN_RACKDEFS" "$(ICPCONFIGROOT)/motionSetPoints/lin_rack_definitions.xml"
-$(IFDMC08) epicsEnvSet "SLOT_DETAILS_FILE" "$(ICPCONFIGROOT)/motionSetPoints/linearsamplechanger.xml"
-$(IFDMC08) epicsEnvSet "LIN_SAMPLE_LKUP_FILE" "$(ICPCONFIGROOT)/motionSetPoints/linsample.txt"
-
-$(IFDMC08) linearSampleChangerConfigure("CURRENT","CURRENT")
-
-$(IFDMC08) dbLoadRecords("$(LINEARSAMPLECHANGER)/db/linearSampleChanger.db","P=$(MYPVPREFIX)LSAMPCHNG:,TARGET_PV1=$(MYPVPREFIX)LKUP:LSAMPLE:RESET,LOOKUP=CURRENT")
--- linearSampleChangerApp/sample_config/NDWXXXX/configurations/galil/sampleChanger.cmd
+++
@@ -1,8 +0,0 @@
-# $(ICPCONFIGDIR) or $(ICPCONFIGROOT) ?
-$(IFDMC05) epicsEnvSet "RACKDEFS" "$(ICPCONFIGROOT)/motionSetPoints/rack_definitions.xml"
-$(IFDMC05) epicsEnvSet "SLOT_DETAILS_FILE" "$(ICPCONFIGROOT)/motionSetPoints/samplechanger.xml"
-$(IFDMC05) epicsEnvSet "SAMPLE_LKUP_FILE" "$(ICPCONFIGROOT)/motionSetPoints/sample.txt"
-
-$(IFDMC05) sampleChangerConfigure("CURRENT","CURRENT")
-
-$(IFDMC05) dbLoadRecords("$(SAMPLECHANGER)/db/sampleChanger.db","P=$(MYPVPREFIX)SAMPCHNG:,TARGET_PV1=$(MYPVPREFIX)LKUP:SAMPLE:RESET,LOOKUP=CURRENT")
