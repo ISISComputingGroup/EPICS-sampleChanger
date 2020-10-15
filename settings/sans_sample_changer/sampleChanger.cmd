f7782c871f489e3fa499c37549113212a88b23d3
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-10-15 18:02:28 +0100

    Use ICPVARDIR

--- settings/sans_sample_changer/sampleChanger.cmd
+++ settings/sans_sample_changer/sampleChanger.cmd
@@ -2,7 +2,7 @@ $(IFIOC_GALIL_01) epicsEnvSet "RACKDEFS" "$(SAMPLECHANGER)/settings/sans_sample_
 $(IFIOC_GALIL_01) epicsEnvSet "SLOT_DETAILS_FILE" "$(SAMPLECHANGER)/settings/sans_sample_changer/samplechanger.xml"
 
 # Don't want this to be in the repo with the other files as it gets automatically generated
-$(IFIOC_GALIL_01) epicsEnvSet "SAMPLE_LKUP_FILE" "c:/instrument/var/tmp/sample.txt"
+$(IFIOC_GALIL_01) epicsEnvSet "SAMPLE_LKUP_FILE" "$(ICPVARDIR)/tmp/sample.txt"
 
 $(IFIOC_GALIL_01) sampleChangerConfigure("CURRENT","CURRENT")
 
